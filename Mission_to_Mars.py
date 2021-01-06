e4a302069fd40287628831beb882d297653deecc
Irene Depacina <i.depacina@gmail.com>
2021-01-05 21:25:28 -0500

    module work.

---
+++ Mission_to_Mars.py
@@ -0,0 +1,71 @@
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager 
+import pandas as pd
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path)
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+slide_elem.find("div", class_='content_title')
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+### Featured Images
+
+# Visit URL
+url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(url)
+
+# Find and click the full image button
+full_image_elem = browser.find_by_id('full_image')
+full_image_elem.click()
+
+# Find the more info button and click that
+browser.is_element_present_by_text('more info', wait_time=1)
+more_info_elem = browser.links.find_by_partial_text('more info')
+more_info_elem.click()
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+# Find the relative image url
+img_url_rel = img_soup.select_one('figure.lede a img').get("src")
+img_url_rel
+
+# Use the base URL to create an absolute URL
+img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
+img_url
+
+# Convert table into a DataFrame
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.columns=['description', 'value']
+df.set_index('description', inplace=True)
+df
+
+# Convert DataFrame to HTML
+df.to_html()
+
+# Close browser
+browser.quit()
+
+
+
---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Define the route for the HTML page
+@app.route("/")
+def index():
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
+
+.update(query_parameter, data, options)
+
+if __name__ == "__main__":
+   app.run()
